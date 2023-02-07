ca618c4031869edb3fbbfb3252111014d3050ec5
Sumit chauhan <sumitchauhan9807666@gmail.com>
2023-02-07 15:00:03 +0530

    woip

--- public/source/js/script.js
+++ public/source/js/script.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
      * The iceServers on this example are public and can be used for your project.
      */
     var peer = new Peer({
-        host: "http://81.181.198.30",
+        host: "81.181.198.30",
         port: 9000,
         path: '/peerjs',
         debug: 3,
